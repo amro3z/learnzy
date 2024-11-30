7b5663bbca519730fe95427cb61180c6be18562b
Mabdelhafiz18 <you@example.com>
2024-11-30 03:45:03 +0200

    updet database

--- learnzy/learnzy/settings.py
+++ learnzy/learnzy/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'learnzy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learnzydb',
+        'USER': 'root',
+        'PASSWORD': '01028642003@Am',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
