6d56cbd74b7bcfeab391892afa8b45800d56f5f7
Mabdelhafiz18 <you@example.com>
2024-12-01 08:43:23 +0200

    update home by adding teacher database

--- learnzy/home/views.py
+++ learnzy/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Courses
+from .models import Teacher
 # Create your views here.
 def index(requset):
     course = Courses.objects.all()
-    x = {'course':course}
-    return render(requset ,'home/inedx.html',x)
+    teacher = Teacher.objects.all()
+    context = {
+        'course':course ,
+        'teacher':teacher
+        }
+    return render(requset ,'home/inedx.html', context)
