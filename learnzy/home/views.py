d2b2963a5e7c87056deffc35899848d88cd54108
Amro <A.hamouda2276@nu.edu.eg>
2024-12-20 04:47:02 +0200

    update the profile page

--- learnzy/home/views.py
+++ learnzy/home/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.timezone import now
 from django.contrib.auth.decorators import login_required
-from .models import Courses, Enrollments
-# from .models import Courses
-from .models import Teacher
-# Create your views here.
+from .models import Courses, Enrollments , Teacher
+
 
 @login_required(login_url='/account/login/')
 def index(requset):
@@ -28,40 +26,37 @@ def course_info(request, pk):
 
 
 def enroll_in_course(request, course_id):
-    course = get_object_or_404(Courses, id=course_id)  # Get the course
-    user_id = request.user.id  # Use user ID as `username` in the model
+    course = get_object_or_404(Courses, id=course_id)  
+    user_id = request.user.id  
 
-    # Check if the user is already enrolled
+    
     if Enrollments.objects.filter(username=user_id, course_id=course_id).exists():
-        return redirect('course', pk=course_id)  # Redirect if already enrolled
+        return redirect('course', pk=course_id)  
 
-    # Add a new enrollment record
     Enrollments.objects.create(
         username=user_id,
         course_id=course_id,
         enrolled_at=now()
     )
 
-    # Redirect to the course page
+   
     return redirect('course', pk=course_id)
 
 def profile(request):
-    # تأكد أن المستخدم مسجل الدخول
+
     if not request.user.is_authenticated:
         return render(request, 'profile/profile.html', {'error': 'You need to log in to view this page.'})
 
-    # المستخدم الحالي
+ 
     user = request.user
 
-    # جلب الكورسات بناءً على username
-    enrollments = Enrollments.objects.filter(username=user.id)  # استخدام username الخاص بالمستخدم
-    enrolled_courses_ids = enrollments.values_list('course_id', flat=True)  # استخرج id الكورسات
-    enrolled_courses = Courses.objects.filter(id__in=enrolled_courses_ids)  # جلب الكورسات المرتبطة باستخدام id
+    enrollments = Enrollments.objects.filter(username=user.id)  
+    enrolled_courses_ids = enrollments.values_list('course_id', flat=True)  
+    enrolled_courses = Courses.objects.filter(id__in=enrolled_courses_ids)  
 
-    # تمرير البيانات إلى القالب
     context = {
-        'user': user,  # بيانات المستخدم
-        'enrolled_courses': enrolled_courses  # الكورسات المرتبطة
+        'user': user, 
+        'enrolled_courses': enrolled_courses  
     }
     return render(request, 'profile/profile.html', context)
 
