44e0df2c335313aff8949a604481698d5d3532ed
Amro <A.hamouda2276@nu.edu.eg>
2024-12-21 07:22:57 +0200

    add and delete course

--- learnzy/home/urls.py
+++ learnzy/home/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns= [
     
-    path('',views.index2, name ='index'),
+    path('',views.index, name ='index'),
     path('enroll/<int:course_id>/', views.enroll_in_course, name='enroll_in_course'),
     path('course/<int:pk>' ,views.course_info, name='course'),
     path('profile/' ,views.profile, name='profile'),
