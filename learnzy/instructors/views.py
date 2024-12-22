a008664d0060c5b2f1bc64ff8c7b439133e8e292
Mabdelhafiz18 <you@example.com>
2024-12-22 02:32:39 +0200

    Add course management functionality for instructors with forms, views, and templates

--- learnzy/instructors/views.py
+++ learnzy/instructors/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from .models import Profile, Courses
-
+from .forms import CourseForm
 def instructor_register_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -41,3 +41,28 @@ def instructor_dashboard_view(request):
     
     courses = Courses.objects.filter(instructor=request.user)
     return render(request, 'instructors/instructor_dashboard.html', {'courses': courses})
+
+
+@login_required
+def add_course_view(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST, request.FILES)
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.instructor = request.user
+            course.save()
+            messages.success(request, 'Course added successfully!')
+            return redirect('instructor_dashboard')
+    else:
+        form = CourseForm()
+    return render(request, 'instructors/add_course.html', {'form': form})
+
+@login_required
+def delete_course_view(request, course_id):
+    course = get_object_or_404(Courses, id=course_id)
+    if course.instructor == request.user:
+        course.delete()
+        messages.success(request, 'Course deleted successfully!')
+    else:
+        messages.error(request, 'You are not authorized to delete this course')
+    return redirect('instructor_dashboard')
