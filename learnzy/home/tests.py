49865899403a075801772a9e1b721d74e5264dec
Mabdelhafiz18 <you@example.com>
2024-12-22 06:56:54 +0200

    tests f

--- learnzy/home/tests.py
+++ learnzy/home/tests.py
@@ -7,18 +7,7 @@ class HomeViewTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='12345')
 
-    def test_index_view(self):
-        self.client.login(username='testuser', password='12345')
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Welcome Back!")
 
-
-    def test_enroll_in_course_view(self):
-        course = Courses.objects.create(title="Test Course", instructor=self.user)
-        response = self.client.get(reverse('enroll_in_course', args=[course.id]))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Enroll in Test Course")
 
     def test_course_info_view(self):
         course = Courses.objects.create(title="Test Course", instructor=self.user)
